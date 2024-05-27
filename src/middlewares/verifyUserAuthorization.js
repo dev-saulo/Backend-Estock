4c637033b379487fa8be4bdca67136476a09ef84
Saulo Christian <134164988+dev-saulo@users.noreply.github.com>
2024-05-27 15:22:37 -0300

    Add files via upload

---
+++ src/middlewares/verifyUserAuthorization.js
@@ -0,0 +1,18 @@
+const AppError = require("../utils/AppError")
+
+//roleToVerify = admin, costumer, sale
+//['admin', 'costumer', 'sale'].includes('sale')
+
+function verifyUserAuthorization(roleToVerify) {
+    return (request, response, next) => {
+        const { role } = request.user;
+
+        if(!roleToVerify.includes(role)) {
+            throw new AppError("Unauthorized", 401);
+        }
+
+        return next();
+    }
+}
+
+module.exports = verifyUserAuthorization;
